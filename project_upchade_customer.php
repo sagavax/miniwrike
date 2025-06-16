8cf9cf29fbad4a46a27c3f8e0b7f0b7184d29b64
Tomas Misura <tmisura@gmail.com>
2025-06-16 21:01:34 +0200

    Fix syntax error when updating project customer

--- project_upchade_customer.php
+++ project_upchade_customer.php
@@ -5,4 +5,6 @@ $customer_id = $_POST['customer_id'];
 $project_id = $_POST['project_id'];
 
 $update_project_customer = "UPDATE projects SET project_customer=$customer_id WHERE project_id=$project_id";
-$result = mysqli_query($db, $update_project_customer or die("MySQLi ERROR: " . mysqli_error($db));
+$result = mysqli_query($db, $update_project_customer) or die("MySQLi ERROR: " . mysqli_error($db));
+
+?>
